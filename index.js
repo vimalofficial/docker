a9c25219701f806472c01a816fa24e3854762ebe
vimalofficial <vimal7pk@gmail.com>
2026-01-29 15:16:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+// API 1
+app.get("/api1", (req, res) => {
+  res.json({ success: true, message: "this is api 1" });
+});
+
+app.get("/api2", (req, res) => {
+  res.json({ success: true, message: "this is api 2 v1" });
+});
+
+app.get("/api3", (req, res) => {
+  res.json({ success: true, message: "this is api 3 v1" });
+});
+
+app.get("/vimal", (req, res) => {
+  res.json({
+    success: true,
+    message: "hello world the devops trigger successfully worked",
+  });
+});
+
+app.get("/workflow", (req, res) => {
+  res.json({
+    success: true,
+    message: "hello world the devops trigger successfully worked",
+  });
+});
+
+// ✅ Docker + Render safe
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
